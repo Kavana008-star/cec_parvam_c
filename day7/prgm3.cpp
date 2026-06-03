86ead7e8508fe994db31fca17629f561774a3486
Varshitha2110 <varshithamanjunath919@gmail.com>
2026-06-03 14:29:36 +0530

    day7

---
+++ day7/prgm3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class student {
+public:
+string name;
+int age;
+student(string n,int a) : name(n), age(a) {}
+void display(){
+    cout<<"(Name: "<<name<<", Age: "<<age<<")"<<endl;
+}
+};
+int main()
+{
+student*s1=new student("Alice",20);
+cout<<"names:"<<s1->name<<endl;
+cout<<"age:"<<s1->age<<endl;
+s1->display();
+(*s1).display();
+delete s1;
+return 0;
+}
